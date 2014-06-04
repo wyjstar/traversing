e92806091e0b67bbc74f1c3e22f56bc88d8db95a
GuanHaihe <guanhaihe@mobartsgame.com>
2014-06-04 15:50:36 +0800

    update

--- shared/utils/const.py
+++ shared/utils/const.py
@@ -2,3 +2,18 @@
 """
 created by server on 14-6-4下午3:04.
 """
+
+class ConstError(Exception):pass
+
+
+class _const(object):
+    """定义常量
+    """
+    def __init__(self, k, v):
+        if k in self.__dict__:
+            raise ConstError
+        else:
+            self.__dict__[k] = v
+
+
+const = _const()
