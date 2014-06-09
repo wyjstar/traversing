aea08345afd5ac292cdda5810f535cfc846e4f1a
GuanHaihe <guanhaihe@mobartsgame.com>
2014-06-09 22:24:37 +0800

    update

--- gameadmin/test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-"""
-created by server on 14-6-9下午6:14.
-"""
-
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return "Hello World"
