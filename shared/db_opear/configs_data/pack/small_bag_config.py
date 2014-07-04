d95861804c75d4dc50951c16ca41619ac0d0cf4a
GuanHaihe <guanhaihe@mobartsgame.com>
2014-07-04 19:20:57 +0800

    掉落大包，掉落小包 实现

---
+++ shared/db_opear/configs_data/pack/small_bag_config.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-7-4下午2:46.
+"""
+
+
+class DropItem(object):
+    """掉落物品
+    """
+    def __init__(self, item_id, item_no, item_type, item_count):
+        self._item_id = item_id  # 掉落ID
+        self._item_no = item_no  # 物品编号
+        self._item_type = item_type  # 物品类型
+        self._item_count = item_count  # 物品数量
+
+
+class SmallBagConfig(object):
+    """掉落小包
+    """
+    def __init__(self):
+        self
+
+
+
+
+
+
+
