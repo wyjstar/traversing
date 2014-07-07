d263592d6eb8d08c70c264a0de276e3a20bee71b
GuanHaihe <guanhaihe@mobartsgame.com>
2014-07-07 11:16:43 +0800

    update

---
+++ app/game/component/character_line_up.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-7-5下午3:07.
+"""
+from app.game.component.Component import Component
+
+
+class CharacterHeroLineUpComponent(Component):
+    """用户英雄阵容组件
+    """
+    def __init__(self):
+
+        self._line_up =
