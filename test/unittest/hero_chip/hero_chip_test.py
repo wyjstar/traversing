07360a934241d23e426197e2b92823bcf4e116f7
wangzhenpu <wangzhenpu@mobartsgame.com>
2014-07-08 10:02:10 +0800

    添加hero_chip_test

--- test/unittest/hero_chip/hero_chip_test.py
+++ test/unittest/hero_chip/hero_chip_test.py
@@ -3,6 +3,12 @@
 created by server on 14-7-7下午8:37.
 """
 import unittest
+from test.unittest.init_test_data import init
+init()
+
+from test.unittest.init_data.mock_hero_chips import player
+from app.game.redis_mode import tb_character_hero_chip
+from app.game.component.character_hero_chip import CharacterHeroChipComponent
 
 
 class HeroChipTest(unittest.TestCase):
@@ -10,5 +16,12 @@ class HeroChipTest(unittest.TestCase):
         pass
 
     def save_data_test(self):
-        pass
+        hero_chip_component = CharacterHeroChipComponent(player)
+        data = tb_character_hero_chip.getObjData(1)
+        self.assertEqual(data.get('id'), 1, "player id error!")
+        num = data.get('hero_chips').get(1000112)
+        self.assertEqual(num, 300, "hero_chip num error!%d_%d" % (num, 300))
+
+
+
 
