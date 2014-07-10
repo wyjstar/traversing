dea2c716aadb90c81ce996f2834c9930b6407808
GuanHaihe <guanhaihe@mobartsgame.com>
2014-07-10 15:42:23 +0800

    update

---
+++ app/game/component/record/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-7-9下午9:53.
+"""
---
+++ app/game/component/record/equipment_enhance.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-7-9下午9:53.
+"""
+from app.game.component.Component import Component
+
+
+class EquipmentEnhanceRecordComponent(Component):
+    """装备
+    """
+
+    def __init__(self, owner):
+        super()
