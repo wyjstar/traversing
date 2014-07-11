94100afc242c495242044117b9cb1ec79f6fb8fd
GuanHaihe <guanhaihe@mobartsgame.com>
2014-07-11 10:51:26 +0800

    update

--- app/game/component/fiance/character_fiance_component.py
+++ app/game/component/fiance/character_fiance_component.py
@@ -8,13 +8,42 @@ from gtwisted.utils import log
 
 
 class CharacterFinanceComponent(Component):
-    """金币"""
+    """货币"""
 
     def __init__(self, owner, coin=0, gold=0, hero_soul=0):
         super(CharacterFinanceComponent, self).__init__(owner)
         self._coin = coin  # 角色的金币
         self._gold = gold  # 角色的充值币
         self._hero_soul = 0  # 角色的武魂
+
+        # TODO 以下没有加到表中
+        self._junior_stone = 0  # 低级熔炼石头
+        self._middle_stone = 0  # 中级熔炼石头
+        self._high_stone = 0  # 高级熔炼石头
+
+    @property
+    def junior_stone(self):
+        return self._junior_stone
+
+    @junior_stone.setter
+    def junior_stone(self, junior_stone):
+        self._junior_stone = junior_stone
+
+    @property
+    def middle_stone(self):
+        return self._middle_stone
+
+    @middle_stone.setter
+    def middle_stone(self, middle_stone):
+        self._middle_stone = middle_stone
+
+    @property
+    def high_stone(self):
+        return self._high_stone
+
+    @high_stone.setter
+    def high_stone(self, high_stone):
+        self._high_stone = high_stone
 
     @property
     def coin(self):
@@ -42,12 +71,11 @@ class CharacterFinanceComponent(Component):
 
     def modify_single_attr(self, attr_name='', num=0, add=True):
         """修改单个属性的值
-        @param attr_name:  属性名称
-        @param num:  修改的数量
+        @param attr_name:  属性名称 str
+        @param num:  修改的数量 int
         @param add: 添加或者减少
         @return:
         """
-        print 'finance modify_single_attr:', attr_name, num, add
         if add:
             setattr(self, attr_name, getattr(self, attr_name, 0) + int(num))
         else:
