b341ca185e3e62352c6ca02ea7fe41b6733c1129
GuanHaihe <guanhaihe@mobartsgame.com>
2014-07-15 14:08:50 +0800

    update

--- app/game/core/line_up_slot.py
+++ app/game/core/line_up_slot.py
@@ -2,13 +2,32 @@
 """
 created by server on 14-7-5下午3:25.
 """
+import cPickle
 
 
 class LineUpSlot(object):
     """卡牌位"""
-    def __init__(self, hero_no, equipment_ids=['']*6):
-        self._hero_no = hero_no
-        self._equipment_ids = equipment_ids
+    def __init__(self, slot_no, activation=False, hero_no=0, equipment_ids=['']*6):
+        self._slot_no = slot_no  # 卡牌位置
+        self._activation = activation  # 激活状态
+        self._hero_no = hero_no  # 英雄编号
+        self._equipment_ids = equipment_ids  # 装备信息
+
+    @property
+    def slot_no(self):
+        return self._slot_no
+
+    @slot_no.setter
+    def slot_no(self, slot_no):
+        self._slot_no = slot_no
+
+    @property
+    def activation(self):
+        return self._activation
+
+    @activation.setter
+    def activation(self, activation):
+        self._activation = activation
 
     @property
     def hero_no(self):
@@ -25,6 +44,25 @@ class LineUpSlot(object):
     @equipment_ids.setter
     def equipment_ids(self, value):
         self._equipment_ids = value
+
+    @property
+    def info(self):
+        """卡牌信息
+        """
+        return dict(hero_no=self._hero_no, equipment_ids=self._equipment_ids)
+
+    @classmethod
+    def loads(cls, data):
+        """解压
+        """
+        info = cPickle.loads(data)
+        slot = cls(**info)
+        return slot
+
+    def dumps(self):
+        """压缩
+        """
+        return cPickle.dumps(self.info)
 
 
 
