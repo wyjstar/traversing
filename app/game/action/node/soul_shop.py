8f0cbe7f9775fcd63055f067abda44ebfbd030ae
wangzhenpu <wangzhenpu@mobartsgame.com>
2014-07-16 15:53:16 +0800

    soul_shop

--- app/game/action/node/soul_shop.py
+++ app/game/action/node/soul_shop.py
@@ -11,43 +11,19 @@ from app.game.core.PlayersManager import PlayersManager
 from app.game.logic.item_group_helper import is_afford, consume, gain, get_return
 from app.proto_file.player_response_pb2 import GameResourcesResponse
 from app.game.core.soul_shop import get_shop_item_ids
+from app.game.logic.soul_shop import soul_shop, get_shop_items
+
 
 @remote_service_handle
 def soul_shop_506(dynamic_id, pro_data):
     """武魂商店"""
-    request = SoulShopRequest()
-    request.ParseFromString(pro_data)
-    game_resources_response = GameResourcesResponse()
-    response = CommonResponse()
-    game_resources_response.res = response
-
-    shop_id = request.id
-    player = PlayersManager().get_player_by_dynamic_id(dynamic_id)
-    shop_item = soul_shop_config.get(shop_id)
-    result = is_afford(player, shop_item.consume)  # 校验
-    if not result.get('result'):
-        response.result = False
-        response.message = '消费不足！'
-    consume(player, shop_item.consume)  # 消耗
-    return_data = gain(player, shop_item.gain)  # 获取
-    extra_return_data = gain(player, shop_item.extra_gain)  # 额外获取
-
-    get_return(player, return_data, game_resources_response)
-    get_return(player, extra_return_data, game_resources_response)
-
-    response.result = True
-    return game_resources_response.SerializeToString()
+    return soul_shop(dynamic_id, pro_data)
 
 
 @remote_service_handle
 def get_shop_items_507(dynamic_id, pro_data=None):
     """获取商品列表"""
-
-    ids = get_shop_item_ids()
-    shop = GetShopItemsResponse()
-    for x in ids:
-        shop.id.append(x)
-    return shop.SerializeToString()
+    return get_shop_items()
 
 
 
