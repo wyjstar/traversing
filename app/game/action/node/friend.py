fcd5244b0f2bc486df4605081f43a642430a12ff
lixin <lixin@mobartsgame.com>
2014-07-19 14:24:48 +0800

    mod

--- app/game/action/node/friend.py
+++ app/game/action/node/friend.py
@@ -2,3 +2,33 @@
 """
 created by server on 14-7-17下午4:36.
 """
+
+from app.game.service.gatenoteservice import remote_service_handle
+
+@remote_service_handle
+def add_friend_request():
+    """
+    :return:
+    """
+    pass
+
+@remote_service_handle
+def add_friend_respond_accept():
+    pass
+
+@remote_service_handle
+def add_friend_respond_refuse():
+    pass
+
+@remote_service_handle
+def del_friend_request():
+    pass
+
+@remote_service_handle
+def add_black_list():
+    pass
+
+@remote_service_handle
+def del_black_list():
+    pass
+
