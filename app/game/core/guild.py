2493caf93001ea0dba6f75134396fdac8ebedc9e
cuichengkai <cuichengkai@mobartsgame.com>
2014-07-22 11:13:33 +0800

    创建公会

--- app/game/core/guild.py
+++ app/game/core/guild.py
@@ -19,33 +19,62 @@ class Guild(object):
         self._exp = exp
         self._fund = fund
         self._call = call
-        self._p_lest = p_list
+        self._p_list = p_list
         self._apple = apple
 
     def create_guild(self, p_id):
         uuid = get_uuid()
-        print 'cuick,AAAAAAAAAAAAAAAAAA,core/guild,01,name:', name
+        self._g_id = uuid
+        self._p_list = {p_id: {'position': 1, \
+                               'contribution': 0, \
+                               'k_num': 0}}
+        print 'cuick,AAAAAAAAAAAAAAAAAA,03,core/guild,01,name:', self._name
         # tb_guild_info
         # tb_character_guild
         # fund 资金
         # position 职位 1会长 2副会长 3长老 4精英 5会员
-        data = {'id': uuid, \
+        # data = {'id': uuid, \
+        #         'base_info': {'name': self._name, \
+        #                       'p_num': self._p_num, \
+        #                       'level': self._level, \
+        #                       'exp': self._exp, \
+        #                       'fund': self._fund}, \
+        #         'call': self._call, \
+        #         'p_list': {p_id: {'position': 1, \
+        #                           'contribution': 0, \
+        #                           'k_num': 0}}, \
+        #         'apply': self._apple}
+        data = {'id': self._g_id, \
                 'info': {'name': self._name, \
-                         'p_num': 1, \
-                         'level': 1, \
-                         'exp': 0, \
-                         'fund': 0, \
-                         'call': '', \
-                         'p_list': {p_id: {'position': 1, \
-                                           'contribution': 0, \
-                                           'k_num': 0}}, \
-                         'apply': {}}}
-        print 'cuick,AAAAAAAAAAAAAAAAAA,core/guild,02,data:', data
+                         'p_num': self._p_num, \
+                         'level': self._level, \
+                         'exp': self._exp, \
+                         'fund': self._fund, \
+                         'call': self._call, \
+                         'p_list': self._p_list, \
+                         'apply': self._apple}}
+        print 'cuick,AAAAAAAAAAAAAAAAAA,04,core/guild,02,data:', data
         tb_guild_info.new(data)
         # 玩家id：公会id
         # 存入
 
+    def save_data(self):
+        data = {
+            'info': {'name': self._name, \
+                     'p_num': self._p_num, \
+                     'level': self._level, \
+                     'exp': self._exp, \
+                     'fund': self._fund, \
+                     'call': self._call, \
+                     'p_list': self._p_list, \
+                     'apply': self._apple}}
 
+        guild_data = tb_guild_info.getObj(self._g_id)
+        guild_data.update_multi(data)
+
+    def init_data(self):
+        data = tb_guild_info.getObjData(self._g_id)
+        print '==========data:', data, '================='
 
 
 
--- app/game/logic/guild.py
+++ app/game/logic/guild.py
@@ -16,8 +16,9 @@ def create_guild(dynamicid, data, **kwargs):
     args = CreateGuildRequest()
     args.ParseFromString(data)
     name = args.name
+    # p_id = player.base_info.id
     p_id = 123456
-
+    print'cuick,AAAAAAAAAAAAAAAAAAAAA,021,logic,p_id:', p_id
     # 判断name合法性，长度,敏感字过滤
 
     # 判断有没有重名
@@ -25,10 +26,12 @@ def create_guild(dynamicid, data, **kwargs):
     # 创建公会
     guild_obj = Guild(name)
     guild_obj.create_guild(p_id)
+    guild_obj.save_data()
+    guild_obj.init_data()
 
     # 返回
     response = CreateGuildResponse()
     res = response.res
     res.result = True
-    print'cuick,AAAAAAAAAAAAAAAAAAAAA,02,logic,name:',name
+    print'cuick,AAAAAAAAAAAAAAAAAAAAA,02,logic,name:', name
     return response.SerializeToString()
