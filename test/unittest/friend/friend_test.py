3537b2e9afd68739499212543d385b6a717034bd
sphinx <sphinx819@gmail.com>
2014-07-24 18:21:02 +0800

    mod

--- test/unittest/friend/friend_test.py
+++ test/unittest/friend/friend_test.py
@@ -2,12 +2,10 @@
 """
 created by server on 14-7-15下午6:30.
 """
-import test.unittest.init_data.init_connection
 import unittest
 from app.game.redis_mode import tb_character_friend
 from app.game.core.PlayersManager import PlayersManager
-from app.game.core.friend import *
-# from app.proto_file.friend_pb2 import *
+from app.game.logic.friend import *
 
 
 class FriendTest(unittest.TestCase):
