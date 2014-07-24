a0b84092429591bbc540937f3a270cbb51326c67
GuanHaihe <guanhaihe@mobartsgame.com>
2014-07-24 15:31:04 +0800

    关卡英雄 怪物属性计算

---
+++ app/game/core/fight/skill.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-7-22下午3:27.
+"""
---
+++ app/game/core/fight/skill_helper.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-7-22下午3:07.
+"""
+
+
+class BuffHelper(object):
+    """BUFF helper
+    """
+    def __init__(self, buffs=[]):
+        self._buffs = buffs
