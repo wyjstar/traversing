a173d050cf1e594e01a0ecd223b0100101647f10
sphinx <sphinx819@gmail.com>
2014-07-25 16:19:45 +0800

    amend friend message

--- app/game/logic/friend.py
+++ app/game/logic/friend.py
@@ -7,75 +7,94 @@ from app.game.logic.common.check import have_player
 from app.game.core.PlayersManager import PlayersManager
 from app.game.core.offline.friend_offline import FriendOffline
 from app.gate.action.root.netforwarding import push_object
+from app.proto_file.common_pb2 import CommonResponse
+from app.proto_file.friend_pb2 import FriendCommon
 
 
 @have_player
-def add_friend_request(dynamic_id, invitee_id, **kwargs):
+def add_friend_request(dynamic_id, data, **kwargs):
     """
     add inviter id to invitee's applicants list
     :param dynamic_id:
     :param inviter_id:
     :return:
     """
+    response = CommonResponse()
+    response.result = 0
+    request = FriendCommon()
+    request.ParseFromString(data)
+
     player = kwargs.get('player')
-    invitee_player = PlayersManager().get_player_by_id(invitee_id)
+    invitee_player = PlayersManager().get_player_by_id(request.target_id)
 
     if invitee_player:
         if not invitee_player.friends.add_applicant(player.base_info.id):
-            return 1  # fail
+            response.result = 1  # fail
+            return response.SerializePartialToString()  # fail
 
         push_object(1010, player.base_info.id, invitee_player.dynamic_id)
     else:
-        friend_offline = FriendOffline(invitee_id)
+        friend_offline = FriendOffline(request.target_id)
         if not friend_offline.add_applicant(player.base_info.id):
-            return 2  # offline fail
+            response.result = 2  # offline fail
+            return response.SerializePartialToString()  # fail
 
     invitee_player.friends.save_data()
 
-    return 0  # fail
+    return response.SerializePartialToString()  # fail
 
 
 @have_player
-def become_friends(dynamic_id, inviter_id, **kwargs):
+def become_friends(dynamic_id, data, **kwargs):
     """
     :param dynamic_id:
     :param inviter_id:
     :param kwargs:
     :return:
     """
+    response = CommonResponse()
+    response.result = 0
+    request = FriendCommon()
+    request.ParseFromString(data)
+
     player = kwargs.get('player')
 
-    if not player.friends.is_in_applicants_list(inviter_id):
-        return 1  # inviter id is not exit in applicants
+    if not player.friends.is_in_applicants_list(request.target_id):
+        response.result = 1  # inviter id is not exit in applicants
+        return response.SerializePartialToString()
 
-    if not player.friends.del_applicant(inviter_id):
-        return 2  # del applicant error
+    if not player.friends.del_applicant(request.target_id):
+        response.result = 2  # del applicant error
+        return response.SerializePartialToString()
 
-    if not player.friends.add_friend(inviter_id):
-        return 3
+    if not player.friends.add_friend(request.target_id):
+        response.result = 3
+        return response.SerializePartialToString()
 
     # save data
     player.friends.save_data()
 
-    inviter_player = PlayersManager().get_player_by_id(inviter_id)
+    inviter_player = PlayersManager().get_player_by_id(request.target_id)
 
     if inviter_player:
         if not inviter_player.friends.add_friend(player.base_info.id):
-            return 4
+            response.result = 4
+            return response.SerializePartialToString()
 
         # save data
         inviter_player.friends.save_data()
         player.friends.save_data()
     else:
-        friend_offline = FriendOffline(inviter_id)
+        friend_offline = FriendOffline(request.target_id)
         if not friend_offline.add_friend(player.base_info.id):
-            return 5
+            response.result = 5
+            return response.SerializePartialToString()
 
-    return 0
+    return response.SerializePartialToString()
 
 
 @have_player
-def refuse_invition(dynamic_id, inviter_id, **kwargs):
+def refuse_invitation(dynamic_id, data, **kwargs):
     """
 
     :param dynamic_id:
@@ -83,20 +102,27 @@ def refuse_invition(dynamic_id, inviter_id, **kwargs):
     :param kwargs:
     :return:
     """
+    response = CommonResponse()
+    response.result = 0
+    request = FriendCommon()
+    request.ParseFromString(data)
+
     player = kwargs.get('player')
 
-    if not player.friends.is_in_applicants_list(inviter_id):
-        return 1
-    if not player.friends.del_applicant(inviter_id):
-        return 2
+    if not player.friends.is_in_applicants_list(request.target_id):
+        response.result = 1
+        return response.SerializePartialToString()
+    if not player.friends.del_applicant(request.target_id):
+        response.result = 2
+        return response.SerializePartialToString()
 
     # save data
     player.friends.save_data()
-    return 0
+    return response.SerializePartialToString()
 
 
 @have_player
-def del_friend(dynamic_id, friend_id, **kwargs):
+def del_friend(dynamic_id, data, **kwargs):
     """
 
     :param dynamic_id:
@@ -104,69 +130,92 @@ def del_friend(dynamic_id, friend_id, **kwargs):
     :param kwargs:
     :return:
     """
+    response = CommonResponse()
+    response.result = 0
+    request = FriendCommon()
+    request.ParseFromString(data)
+
     player = kwargs.get('player')
 
-    if not player.friends.is_friend(friend_id):
-        return 1  # there is no friend with friend id
+    if not player.friends.is_friend(request.target_id):
+        response.result = 1  # there is no friend with friend id
+        return response.SerializePartialToString()
 
-    if not player.friends.del_friend(friend_id):
-        return 2
+    if not player.friends.del_friend(request.target_id):
+        response.result = 2
+        return response.SerializePartialToString()
 
     # save data
     player.friends.save_data()
 
-    friend_player = PlayersManager().get_player_by_id(friend_id)
+    friend_player = PlayersManager().get_player_by_id(request.target_id)
     if friend_player:
         if not friend_player.friends.is_friend(player.base_info.id):
-            return 3
+            response.result = 3
+            return response.SerializePartialToString()
         if not friend_player.friends.del_friend(player.base_info.id):
-            return 4
+            response.result = 4
+            return response.SerializePartialToString()
     else:
-        friend_offline = FriendOffline(friend_id)
+        friend_offline = FriendOffline(request.target_id)
         if not friend_offline.del_friend(player.base_info.id):
-            return 5
+            response.result = 5
+            return response.SerializePartialToString()
 
-    return 0
+    return response.SerializePartialToString()
 
 
 @have_player
-def add_player_to_blacklist(dynamic_id, target_id, **kwargs):
+def add_player_to_blacklist(dynamic_id, data, **kwargs):
     """
 
     :param dynamic_id:
     :param target_id:
     :return:
     """
+    response = CommonResponse()
+    response.result = 0
+    request = FriendCommon()
+    request.ParseFromString(data)
+
     player = kwargs.get('player')
 
-    if player.friends.is_in_blacklist(target_id):
-        return 1  # already exist the player in blacklist
+    if player.friends.is_in_blacklist(request.target_id):
+        response.result = 1  # already exist the player in blacklist
+        return response.SerializePartialToString()
 
-    if not player.friends.add_blacklist(target_id):
-        return 2
+    if not player.friends.add_blacklist(request.target_id):
+        response.result = 2
+        return response.SerializePartialToString()
 
     # save data
     player.friends.save_data()
-    return 0
+    return response.SerializePartialToString()
 
 
 @have_player
-def del_player_from_blacklist(dynamic_id, target_id, **kwargs):
+def del_player_from_blacklist(dynamic_id, data, **kwargs):
     """
 
     :param dynamic_id:
     :param target_id:
     :return:
     """
+    response = CommonResponse()
+    response.result = 0
+    request = FriendCommon()
+    request.ParseFromString(data)
+
     player = kwargs.get('player')
 
-    if not player.friends.is_in_blacklist(target_id):
-        return 1  # not exist the player in blacklist
+    if not player.friends.is_in_blacklist(request.target_id):
+        response.result = 1  # not exist the player in blacklist
+        return response.SerializePartialToString()
 
-    if not player.friends.del_blacklist(target_id):
-        return 2
+    if not player.friends.del_blacklist(request.target_id):
+        response.result = 2
+        return response.SerializePartialToString()
 
     # save data
     player.friends.save_data()
-    return 0
-
+    return response.SerializePartialToString()
