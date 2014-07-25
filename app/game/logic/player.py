7c4568193853aae8f189260fadf3f24c7154876b
wangzhenpu <wangzhenpu@mobartsgame.com>
2014-07-25 18:11:48 +0800

    fix bug

--- app/game/logic/player.py
+++ app/game/logic/player.py
@@ -3,7 +3,6 @@
 created by server on 14-7-21下午5:12.
 """
 
-from app.gate.core.users_manager import UsersManager
 from app.game.logic.common.check import have_player
 from app.game.redis_mode import tb_nickname_mapping
 from app.proto_file.common_pb2 import CommonResponse
@@ -18,7 +17,7 @@ def nickname_create(dynamic_id, nickname, **kwargs):
     if data:
         response.result = False
         return response.SerializeToString()
-    
+
     nickname_data = dict(id=player.base_info.id, nickname=nickname)
     nickname_mmode = tb_nickname_mapping.new(nickname_data)
     nickname_mmode.insert()
