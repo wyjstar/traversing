4901f8840fc51ce96cf84fccc8686eaafebabcf4
sphinx <sphinx819@gmail.com>
2014-07-28 18:02:33 +0800

    friend test fix

--- app/game/action/root/netforwarding.py
+++ app/game/action/root/netforwarding.py
@@ -1,15 +1,14 @@
 #coding:utf8
 """
 """
-from gfirefly.server.globalobject import rootserviceHandle
 from gfirefly.server.globalobject import GlobalObject
 
 
-@rootserviceHandle
 def push_object(topic_id, msg, send_list):
     """ send msg to client in send_list
         send_list:
     """
-    GlobalObject().remotep['gate'].callRemote("pushObject", topic_id, msg, send_list)
+    if 'gate' in GlobalObject().remote.keys():
+        GlobalObject().remote['gate'].callRemote("pushObject", topic_id, msg, send_list)
 
 
--- gfirefly/management/commands/stopservice.py
+++ gfirefly/management/commands/stopservice.py
@@ -1,26 +1,27 @@
-#coding:utf8
+# coding:utf8
 '''
 Created on 2013-8-11
 
 @author: lan (www.9miao.com)
 '''
-import urllib,sys
+import urllib, sys
+
 
 def execute(*args):
     """
     """
     if not args:
-        masterport =9998
+        masterport = 9998
         hostname = "localhost"
     else:
-        if len(args)>1:
+        if len(args) > 1:
             hostname = args[0]
             masterport = int(args[1])
         else:
             hostname = "localhost"
             masterport = int(args[0])
-        
-    url = "http://%s:%s/stop"%(hostname, masterport)
+
+    url = "http://%s:%s/stop" % (hostname, masterport)
     try:
         response = urllib.urlopen(url)
     except:
