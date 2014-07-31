f0cc88c813ea154f6758f58ef69377b6dff0cee1
wangzhenpu <wangzhenpu@mobartsgame.com>
2014-07-31 15:55:59 +0800

    添加部分注释

--- tool/proto_to_one.py
+++ tool/proto_to_one.py
@@ -5,6 +5,9 @@ created by server on 14-7-25下午5:52.
 
 root_path = "../app/proto_file/proto/"
 import os
+os.remove("traversing_one.proto")
+
+
 result_file = open("traversing_one.proto", "a")
 for file_name in os.listdir(root_path):
     file_path = root_path + file_name
