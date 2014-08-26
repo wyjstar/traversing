3a4e961b3c2dd2340cb55d9eed8f5ea3249a2329
GuanHaihe <guanhaihe@mobartsgame.com>
2014-08-26 22:00:24 +0800

    热更新 配置数据

---
+++ app/game/doreload.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-8-26下午7:14.
+"""
+from gtwisted.utils import log
+from shared.db_opear.configs_data import game_configs
+from shared.utils.xreload import xreload
+
+
+log.msg('test reload')
+
+print game_configs.base_config
+print '----------------------'
+xreload(game_configs)
+print '----------------------'
+
+
+
