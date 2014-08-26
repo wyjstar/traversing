ae0207173476710f4d33c7e36b5c4e2292a81214
lixin <lixin@mobartsgame.com>
2014-08-26 15:31:11 +0800

    add activity config

--- shared/db_opear/configs_data/game_configs.py
+++ shared/db_opear/configs_data/game_configs.py
@@ -22,6 +22,7 @@ from shared.db_opear.configs_data.skill_config import SkillConfig
 from shared.db_opear.configs_data.stage_config import StageConfig
 from shared.db_opear.configs_data.soul_shop_config import SoulShopConfig
 from shared.db_opear.configs_data.warriors_config import WarriorsConfig
+from shared.db_opear.configs_data.activity_config import ActivityConfig
 
 
 print id(dbpool)
@@ -38,8 +39,9 @@ def init():
     port = 8066  #  3306 是MySQL服务使用的TCP端口号，一般默认是3306
     dbname = "db_traversing"  #  要使用的数据库库名
     charset = "utf8"  #  要使用的数据库的编码
+    ##firefly重新封装的连接数据库的方法，这一步就是初始化数据库连接池，这样你就可连接到你要使用的数据库了
     dbpool.initPool(host=hostname, user=user, passwd=password, port=port, db=dbname,
-                    charset=charset)  ##firefly重新封装的连接数据库的方法，这一步就是初始化数据库连接池，这样你就可连接到你要使用的数据库了
+                    charset=charset)
 
 init()
 
@@ -82,6 +84,7 @@ skill_config = {}
 skill_buff_config = {}
 guild_config = {}
 warriors_config = {}
+activity_config = {}
 
 
 all_config_name = {
@@ -106,6 +109,7 @@ all_config_name = {
     'guild_config': GuildConfig(),
     'soul_shop_config': SoulShopConfig(),
     'warriors_config': WarriorsConfig(),
+    'activity_config': ActivityConfig(),
 }
 
 
@@ -125,7 +129,7 @@ for config_name in all_config_name.keys():
 
 if __name__ == '__main__':
     init()
-    print warriors_config
+    print activity_config
 
 
 
