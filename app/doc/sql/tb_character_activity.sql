6d48d8398524b3efd1406c821896255bd688fb30
cuichengkai <cuichengkai@mobartsgame.com>
2014-08-29 14:40:23 +0800

    activity

--- app/doc/sql/tb_character_activity.sql
+++ app/doc/sql/tb_character_activity.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS `tb_character_activity`;
 CREATE TABLE `tb_character_activity` (
   `id` bigint(20) NOT NULL,
   `sign_in` blob,
+  `feast` bigint(20),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
