eed8e8275065b14b81723106751f057d1947b647
cuichengkai <cuichengkai@mobartsgame.com>
2014-09-02 18:08:56 +0800

    fix bug

--- test/init_data/mock_equipment.py
+++ test/init_data/mock_equipment.py
@@ -7,37 +7,37 @@ from app.game.core.PlayersManager import PlayersManager
 
 
 def init_equipment(player):
-    equipment_obj = Equipment('0001', '', 100001)
+    equipment_obj = Equipment('0001%s' % player.base_info.id, '', 100001)
     player.equipment_component.add_exist_equipment(equipment_obj)
     equipment_obj.attribute.strengthen_lv = 2
     equipment_obj.attribute.awakening_lv = 2
     equipment_obj.save_data()
 
-    equipment_obj = Equipment('0002', '', 100002)
+    equipment_obj = Equipment('0002%s' % player.base_info.id, '', 100002)
     player.equipment_component.add_exist_equipment(equipment_obj)
     equipment_obj.attribute.strengthen_lv = 2
     equipment_obj.attribute.awakening_lv = 2
     equipment_obj.save_data()
 
-    equipment_obj = Equipment('0003', '', 100003)
+    equipment_obj = Equipment('0003%s' % player.base_info.id, '', 100003)
     player.equipment_component.add_exist_equipment(equipment_obj)
     equipment_obj.attribute.strengthen_lv = 2
     equipment_obj.attribute.awakening_lv = 2
     equipment_obj.save_data()
 
-    equipment_obj = Equipment('0004', '', 100004)
+    equipment_obj = Equipment('0004%s' % player.base_info.id, '', 100004)
     player.equipment_component.add_exist_equipment(equipment_obj)
     equipment_obj.attribute.strengthen_lv = 2
     equipment_obj.attribute.awakening_lv = 2
     equipment_obj.save_data()
 
-    equipment_obj = Equipment('0005', '', 100005)
+    equipment_obj = Equipment('0005%s' % player.base_info.id, '', 100005)
     player.equipment_component.add_exist_equipment(equipment_obj)
     equipment_obj.attribute.strengthen_lv = 2
     equipment_obj.attribute.awakening_lv = 2
     equipment_obj.save_data()
 
-    equipment_obj = Equipment('0006', '', 100006)
+    equipment_obj = Equipment('0006%s' % player.base_info.id, '', 100006)
     player.equipment_component.add_exist_equipment(equipment_obj)
     equipment_obj.attribute.strengthen_lv = 2
     equipment_obj.attribute.awakening_lv = 2
