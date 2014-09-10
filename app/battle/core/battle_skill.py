ee72ea1ceb997adb9835d22709544b8988e53c96
sunpang <sunpang@mobartsgame.com>
2014-09-10 18:55:23 +0800

    update battle frame

---
+++ app/battle/core/battle_skill.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-9-10下午5:32.
+"""
+
+class BattleSkill(object):
+    """战斗内技能
+    """
+    def __init__(self):
+        """
+        """
+        pass
+
+    # @property
+    # def no(self):
+    #     return self._no
+    #
+    # @no.setter
+    # def no(self, no):
+    #     self._no = no
