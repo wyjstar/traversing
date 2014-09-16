d4a91491c8ba7a152f5ce0fcd7d72f7cd1cb2e9d
sunpang <sunpang@mobartsgame.com>
2014-09-16 15:55:29 +0800

    battle_check and Round define

---
+++ app/game/logic/battle.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-9-15上午11:32.
+"""
+from app.game.logic.common.check import have_player
+from app.proto_file.battle_round_pb2 import Round
+
+@have_player
+def get_round(dynamic_id, **kwargs):
+    player = kwargs.get('player')
+    requ = Round()
+    for hero in player.hero_component.get_heros():
+        hero_pb = response.heros.add()
+        hero.update_pb(hero_pb)
+    return response.SerializePartialToString()
