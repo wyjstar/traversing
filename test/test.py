88c448c7815cefec4194ba284fd9d59ec1ed113d
zhizouxiao <zhizouxiao@gmail.com>
2014-10-10 21:59:23 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,11 +1,9 @@
 # -*- coding:utf-8 -*-
 """
-created by server on 14-7-23下午3:45.
 """
-import time
-from app.proto_file.common_pb2 import CommonResponse
 if __name__ == '__main__':
     str = '但是感到舒服'
 if __name__ == '__main__':
     print 1
+print 1
 
