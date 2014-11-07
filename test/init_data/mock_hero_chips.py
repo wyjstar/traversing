10ad5cd755da115749b906ac4eabc8197f3a26a8
wangzhenpu <wangzhenpu@mobartsgame.com>
2014-11-07 16:15:03 +0800

    change init data

--- test/init_data/mock_hero_chips.py
+++ test/init_data/mock_hero_chips.py
@@ -5,10 +5,15 @@ created by server on 14-7-7下午5:29.
 
 from app.game.core.PlayersManager import PlayersManager
 from app.game.core.hero_chip import HeroChip
-from app.game.redis_mode import tb_character_hero_chip
-
+from shared.db_opear.configs_data.game_configs import chip_config
 
 def init_hero_chip(player):
+    for k, val in chip_config.get('chips').items():
+        if val.get('type') == 2: continue
+        chip = HeroChip(int(k), 400)
+        player.hero_chip_component.add_chip(chip)
+    return
+
     hero_chip1 = HeroChip(1010020, 300)
     hero_chip2 = HeroChip(1010021, 300)
 
