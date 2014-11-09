f06057ffb9b93a4a93916bcac767610241d5621c
lixin <lixin@mail.com>
2014-11-09 21:53:42 +0800

    fix

--- gfirefly/server/logobj.py
+++ gfirefly/server/logobj.py
@@ -5,10 +5,46 @@ modify on 2014-10-8
 """
 import sys
 import logging
+from logging import Formatter
 from logging.handlers import DatagramHandler
 from shared.utils import const
 
 logger_name = 's'
+
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+MAGENTA = '\033[35m'
+OCEAN = '\033[36m'
+RESET = '\033[0m'
+
+color_map = {}
+color_map[logging.DEBUG] = YELLOW
+color_map[logging.ERROR] = RED
+color_map[logging.INFO] = OCEAN
+color_map[logging.CRITICAL] = MAGENTA
+
+
+class MyFormatter(Formatter):
+    def format(self, record):
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+        s = self._fmt % record.__dict__
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            try:
+                s = s + record.exc_text
+            except UnicodeError:
+                s = s + record.exc_text.decode(sys.getfilesystemencoding(),
+                                               'replace')
+        return color_map[record.levelno] + s
 
 
 def log_init(log_path):
@@ -17,17 +53,16 @@ def log_init(log_path):
     file_name = str(log_path).split('/')[-1].split('.')[0]
     datefmt = '%Y-%m-%d %I:%M:%S %p'
     fmt = '%(asctime)s-[%(levelname)s]-[' + file_name + ']: %(message)s'
-    formatter = logging.Formatter(fmt, datefmt)
 
     _logger.setLevel(logging.DEBUG)
     fh = logging.FileHandler(log_path)
     fh.setLevel(logging.INFO)
-    fh.setFormatter(formatter)
+    fh.setFormatter(Formatter(fmt, datefmt))
     _logger.addHandler(fh)
 
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.DEBUG)
-    ch.setFormatter(formatter)
+    ch.setFormatter(MyFormatter(fmt, datefmt))
     _logger.addHandler(ch)
 
     uh = DatagramHandler(const.const.TLOG_ADDR[0], const.const.TLOG_ADDR[1])
