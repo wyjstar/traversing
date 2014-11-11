992923452a101bae27bb760c8269bbf84bec7fc3
lixin <lixin@mail.com>
2014-11-11 22:23:56 +0800

    brew

---
+++ app/game/component/brew/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+"""
+created by lucas on 14-11-11下午4:05.
+"""
---
+++ app/game/component/brew/brew.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+created by lucas on 14-11-11下午4:05.
+"""
+from app.game.component.Component import Component
+
+
+class BrewComponent(Component):
+    def __init__(self, owner):
+        super(BrewComponent, self).__init__(owner)
+
+    def init_data(self):
+        pass
+
+    def save_data(self):
+        pass
