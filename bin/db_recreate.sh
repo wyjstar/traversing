53fa932f2032869f2b240dc89fa6c2fa925a8b0b
panjiang <panjiang@mobartsgame.com>
2014-11-14 14:22:29 +0800

    bin: shell script

---
+++ bin/db_recreate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+databases_dir="app/doc/databases"
+cobar_connect="mysql -h127.0.0.1 -utest -ptest -P8066 -Ddb_traversing"
+
+echo "rebuild acccount database"
+eval "$cobar_connect < $databases_dir/account.sql"
+
+echo "rebuild traversing database"
+eval "$cobar_connect < $databases_dir/traversing.sql"
+eval "$cobar_connect < $databases_dir/traversing_master.sql"
+
+echo "done"
+exit 0
