86f75a7bc12d467c3d5aa668c650a38b4ed82dbc
wangzhenpu <wangzhenpu@mobartsgame.com>
2014-12-14 21:06:37 +0800

    bug fix

--- app/game/action/node/inherit.py
+++ app/game/action/node/inherit.py
@@ -14,6 +14,7 @@ def inherit_refine_151(pro_data, player):
     炼体传承
     """
     request = inherit_pb2.InheritRefineRequest()
+    request.ParseFromString(pro_data)
     origin_id = request.origin
     target_id = request.target
 
@@ -25,14 +26,17 @@ def inherit_refine_151(pro_data, player):
     if not origin or (not target):
         logger.error("hero %s or %s not exists" % (origin_id, target_id))
         response.result = False
+        return response.SerializeToString()
 
     if not origin.refine:
         logger.error("origin hero %s do not have refine!" % origin_id)
         response.result = False
+        return response.SerializeToString()
 
     if origin.refine <= target.refine:
         logger.error("origin hero %s refine <= target hero %s refine!" % (origin_id, target_id))
         response.result = False
+        return response.SerializeToString()
 
     target.refine = origin.refine
     origin.refine = 0
@@ -50,6 +54,7 @@ def inherit_equipment_152(pro_data, player):
     装备传承
     """
     request = inherit_pb2.InheritEquipmentRequest()
+    request.ParseFromString(pro_data)
     origin_id = request.origin
     target_id = request.target
 
@@ -61,20 +66,23 @@ def inherit_equipment_152(pro_data, player):
     if not origin or (not target):
         logger.error("equip %s or %s not exists" % (origin_id, target_id))
         response.result = False
+        return response.SerializeToString()
 
     if origin.attribute.strengthen_lv <= target.attribute.strengthen_lv:
         logger.error("origin equip %s strengthen_lv <= target equip %s strengthen_lv!" % (origin_id, target_id))
         response.result = False
+        return response.SerializeToString()
 
     if origin.equipment_config_info.get("quality") != target.equipment_config_info.get("quality"):
         logger.error("origin.quality!=target.quality")
         response.result = False
+        return response.SerializeToString()
 
     target.attribute.strengthen_lv = origin.attribute.strengthen_lv
     origin.attribute.strength_lv = 1
     # 传承强化过程
-    target.enhance_record = origin.enhance_record
-    origin.enhance_record = []
+    target.enhance_record.enhance_record = origin.enhance_record.enhance_record
+    origin.enhance_record.enhance_record = []
     target.save_data()
     origin.save_data()
 
@@ -88,7 +96,8 @@ def inherit_upara_153(pro_data, player):
     """
     无双传承
     """
-    request = inherit_pb2.InheritEquipmentRequest()
+    request = inherit_pb2.InheritUnparaRequest()
+    request.ParseFromString(pro_data)
     origin_id = request.origin
     target_id = request.target
 
@@ -100,10 +109,12 @@ def inherit_upara_153(pro_data, player):
     if not origin_level or (not target_level):
         logger.error("unpara %s or %s not exists" % (origin_id, target_id))
         response.result = False
+        return response.SerializeToString()
 
     if origin_level <= target_level:
         logger.error("origin.level!=target.level")
         response.result = False
+        return response.SerializeToString()
 
     player.line_up_component.unpars[target_id] = origin_id
     player.line_up_component.unpars[origin_id] = 1
