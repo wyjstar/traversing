34a7df360414e8787de94e582e630f12a8390c78
wangzhenpu <wangzhenpu@mobartsgame.com>
2014-12-29 14:10:18 +0800

    status

---
+++ app/game/component/fight/hero_attr_cal/combat_power.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+"""
+created by server on 14-12-29下午2:03.
+"""
+
+def combat_power_hero(player, hero_no):
+    pass
+
+def combat_power_hero_lineup(player, hero_no):
+    pass
