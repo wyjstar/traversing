53f60eaf786418300ac08d5dcf74acc92bc81d15
lixin <lixin@mail.com>
2014-12-30 21:51:57 +0800

    fix

--- app/transit/action/local/__init__.py
+++ app/transit/action/local/__init__.py
@@ -1,5 +1,10 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 """
-created by sphinx on 
+created by sphinx on
 """
 import message
+import pvp_award
+from gtwisted.core import reactor
+
+
+reactor.callLater(10, pvp_award.pvp_award_tick)
--- app/transit/action/local/message.py
+++ app/transit/action/local/message.py
@@ -9,18 +9,25 @@ groot = GlobalObject().root
 
 
 @rootserviceHandle
-def push_message_remote(key, character_id, args, kw):
-    message_cache.cache(key, character_id, *args, **kw)
+def push_message_remote(key, character_id, args):
+    childs = groot.childsmanager.childs
+    for child in childs.values():
+        if 'gate' in child.name:
+            result = child.pull_message_remote(*args)
+            if type(result) is bool and result:
+                return
+
+    message_cache.cache(key, character_id, *args)
     return True
 
 
 @rootserviceHandle
 def pull_message_remote(character_id):
     count = 0
-    for key, message in message_cache.get(character_id):
-        childs = groot.childsmanager.childs
-        # print groot.childsmanager
+    childs = groot.childsmanager.childs
+    # print groot.childsmanager
 
+    for key, message in message_cache.get(character_id):
         args = (message.get('topic_id'), message.get('character_id'))
         args += message.get('args')
         kw = message.get('kw')
