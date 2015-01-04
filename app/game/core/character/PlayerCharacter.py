ea340a6532acc4b0ca31d3da71ae437e30132846
lixin <lixin@mobartsgame.com>
2015-01-04 15:47:48 +0800

    fix

--- app/game/core/character/PlayerCharacter.py
+++ app/game/core/character/PlayerCharacter.py
@@ -138,7 +138,7 @@ class PlayerCharacter(Character):
         self._line_up.init_data()
         self._equipment.init_data()
         self._equipment_chip.init_data(character_info)
-        self._hero_chip_component.init_hero_chips()  # 初始化武将碎片
+        self._hero_chip_component.init_data(character_info)  # 初始化武将碎片
         self._mail.init_data()  # 初始化邮箱
         self._friends.init_data(character_info)
         self._guild.init_data(character_info)
