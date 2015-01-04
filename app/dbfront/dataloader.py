0521e81298ebd8f966e4294d831963180c2cfc09
lixin <lixin@mobartsgame.com>
2015-01-04 17:20:25 +0800

    fix

--- app/dbfront/dataloader.py
+++ app/dbfront/dataloader.py
@@ -15,29 +15,22 @@ def register_madmin():
     MAdminManager().registe(memmode.tb_character_info)
     # 英雄信息表
     MAdminManager().registe(memmode.tb_character_hero)
-    # 用户英雄碎片信息表
-    MAdminManager().registe(memmode.tb_character_hero_chip)
     # 用户道具背包
     MAdminManager().registe(memmode.tb_character_item_package)
     # 帐号表
     MAdminManager().registe(memmode.tb_account)
     # 用户阵容信息
     MAdminManager().registe(memmode.tb_character_line_up)
-    # 装备碎片表
-    MAdminManager().registe(memmode.tb_character_equipment_chip)
     # 装备信息表
     MAdminManager().registe(memmode.tb_equipment_info)
     # 公会信息表
     MAdminManager().registe(memmode.tb_guild_info)
     # 玩家公会表
     MAdminManager().registe(memmode.tb_guild_name)
-    # 玩家活动表
-    MAdminManager().registe(memmode.tb_character_activity)
     # 关卡信息表
     MAdminManager().registe(memmode.tb_character_stages)
     # 邮件表
     MAdminManager().registe(memmode.tb_mail_info)
-
     # 活跃度
     MAdminManager().registe(memmode.tb_character_tasks)
 
