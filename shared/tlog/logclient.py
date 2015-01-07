62ccb08df8b3fed1a58ddf7f2fd0291a13687d22
cuichengkai <cuichengkai@mobartsgame.com>
2015-01-07 15:56:55 +0800

    tlog

---
+++ shared/tlog/logclient.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+# from gfirefly.server.globalobject import GlobalObject
+import socket
+# from gfirefly.server.logobj import logger
+# host = GlobalObject().json_config['tlog']['host']
+# port = GlobalObject().json_config['tlog']['port']
+host = "192.169.10.26"
+port = 30005
+
+
+class LogClient:
+    """
+    for tlog, UDP
+    """
+    sock = None
+
+    def __init__(self):
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except socket.error, arg:
+            (_, err_msg) = arg
+            # logger.info(str(err_msg))
+            print str(err_msg)
+
+    def send_msg(self, msg):
+        if msg:
+            try:
+                self.sock.sendto(msg, (host, port))
+            except socket.error, arg:
+                (_, err_msg) = arg
+                # logger.info(str(err_msg))
+                print str(err_msg)
+
+udphandler = LogClient()
+
+
+def gethandler():
+    return udphandler
+
+if "__main__" == __name__:
+    gethandler().send_msg('hello world\n')
