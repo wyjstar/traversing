2e7628038bd53d35b9218be10c61b32105073fe1
lixin <lixin@mobartsgame.com>
2015-03-10 16:27:51 +0800

    fix

--- app/game/core/character/PlayerCharacter.py
+++ app/game/core/character/PlayerCharacter.py
@@ -72,8 +72,7 @@ class PlayerCharacter(object):
                      character_info['id'],
                      character_info['level'])
         char_obj.new(character_info)
-        char_all = tb_character_info.getObj('all')
-        char_all.sadd(self._pid)
+        tb_character_info.sadd('all', self._pid)
 
     @property
     def character_id(self):
