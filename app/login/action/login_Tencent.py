3621093ec4fb240bb0e2a4182f4b56fb19c65d32
wangzhenpu <wangzhenpu@mobartsgame.com>
2015-03-12 18:40:20 +0800

    bug fix

--- app/login/action/login_Tencent.py
+++ app/login/action/login_Tencent.py
@@ -4,7 +4,6 @@ created by sphinx on
 """
 import json
 import uuid
-import urllib
 from flask import request
 from app.login.model.manager import account_cache, server_manager
 from gfirefly.server.globalobject import webserviceHandle
@@ -21,12 +20,6 @@ qq_appid = 1104297231
 qq_appkey = 'y33yRx3FveVZb1dw'
 wx_appid = 'wxf77437c08cb06196'
 wx_appkey = '8274b9e862581f8b4976ba90ad2d4b77'
-
-pay_host = ('10.142.22.11', 8080)
-goods_host = ('10.142.22.11', 8080)
-valid_host = ('10.130.2.233', 80)
-wx_rank_host = ('api.weixin.qq.com', 80)
-qq_rank_host = ('10.153.96.115', 10000)
 
 @webserviceHandle('/login')
 def server_login():
@@ -55,9 +48,9 @@ def __login(platform, openid, access_token):
     logger.debug('player login openid:%s access_token %s' % (openid, access_token))
     log = logger_sdk.new_log('TxApi')
     msdk = Msdk(host, qq_appid, qq_appkey, wx_appid, wx_appkey, log=log)
-    res = msdk.verify_login(platform, openid, access_token)
+    res = msdk.verify_login(int(platform), openid, access_token)
     logger.debug(res)
     logger.debug(res)
-    if res.get('ret') == 0:
+    if res == 0:
         return str({'result': True, 'account_id': '\'%s\'' % openid})
     return str({'result': False})
