1869fb6d63e1d456a963e8e5799d01670f2a26a3
lixin <lixin@mobartsgame.com>
2015-03-19 20:51:43 +0800

    fix

--- app/game/action/node/player.py
+++ app/game/action/node/player.py
@@ -151,7 +151,7 @@ def new_guide_step_1802(data, player):
         response.res.result = False
         return response.SerializePartialToString()
 
-    player.base_info.newbee_guide_id = new_guide_item.get('backID')
+    player.base_info.newbee_guide_id = request.step_id
     player.base_info.save_data()
     response.res.result = True
 
