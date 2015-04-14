98da857cc00aee80dd53824e462b5021d966df80
wangzhenpu <wangzhenpu@mobartsgame.com>
2015-04-14 17:27:49 +0800

    bug fix

--- deploy/create_db.sh
+++ deploy/create_db.sh
@@ -6,7 +6,7 @@ echo "start ....."
 
 
 echo "create mysql db & table..."
-mysql -h127.0.0.1 -uroot -p123456 -Ddb_traversing<../app/doc/databases/account.sql
+mysql -uroot -p123456 -Ddb_traversing<../app/doc/databases/account.sql
 redis-cli flushall
 
 echo "clear mysql db & redis"
