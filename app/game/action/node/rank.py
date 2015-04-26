df9e2bb6625c06b02311a1070110eb2d15ae2ab7
cuichengkai <cuichengkai@mobartsgame.com>
2015-04-26 12:03:01 +0800

    rank

---
+++ app/game/action/node/rank.py
@@ -0,0 +1,110 @@
+# -*- coding:utf-8 -*-
+"""
+created by K.
+"""
+from gfirefly.server.globalobject import remoteserviceHandle
+from gfirefly.server.globalobject import GlobalObject
+from shared.db_opear.configs_data import game_configs
+from app.game.redis_mode import tb_guild_info, tb_guild_name
+from app.game.redis_mode import tb_character_info
+from gfirefly.server.logobj import logger
+from app.proto_file import rank_pb2
+from shared.utils.const import const
+
+
+remote_gate = GlobalObject().remote['gate']
+
+
+@remoteserviceHandle('gate')
+def get_rank_info_1805(data, player):
+    """查看排行信息 """
+    args = rank_pb2.GetRankResponse()
+    args.ParseFromString(data)
+    first_no = args.first_no
+    last_no = args.last_no
+    rank_type = args.rank_type
+
+    response = rank_pb2.GetRankResponse()
+
+    mid = player.base_info.id
+    if rank_type == 3:
+        get_level_rank(first_no, last_no, player, response)
+    elif rank_type == 1:
+        get_power_rank(first_no, last_no, player, response)
+    elif rank_type == 2:
+        get_star_rank(first_no, last_no, player, response)
+    else:
+        response.res.result = False
+        response.res.result_no = 838
+
+    return response.SerializeToString()
+
+
+def get_level_rank(first_no, last_no, player, response):
+    if flag_doublu_day():
+        rank_name = 'LevelRank2'
+        last_rank_name = 'LevelRank1'
+    else:
+        rank_name = 'LevelRank1'
+        last_rank_name = 'LevelRank2'
+
+    ranks = remote_gate.get_rank_remote(rank_name, first_no, last_no)
+    rank_num = first_no
+    for pid, rankinfo in ranks.items():
+        res_user_info = response.user_info.add()
+        res_user_info.id = pid
+        res_user_info.level = rankinfo/const.rank_xs
+        res_user_info.fight_power = rankinfo % const.rank_xs
+        res_user_info.rank = rank_num
+
+        character_obj = tb_character_info.getObj(pid)
+        character_info = character_obj.hmget(['nickname'])
+        res_user_info.nickname = character_info['nickname']
+        rank_num += 1
+
+    if first_no == 1:
+        rank_no = remote_gate.get_rank_remote(rank_name, player.base_info.id)
+        if rank_no:
+            ranks = remote_gate.get_rank_remote(rank_name, rank_no, rank_no)
+            res_my_rank_info = response.my_rank_info
+            res_my_rank_info.rank = rank_no
+            res_my_rank_info.level = ranks[player.base_info.id]/const.rank_xs
+            res_my_rank_info.fight_power =\
+                ranks[player.base_info.id] % const.rank_xs
+
+            last_rank_no = remote_gate.get_rank_remote(last_rank_name, player.base_info.id)
+            if last_rank_no:
+                res_my_rank_info.last_rank = last_rank_no
+        # 前100名有多少人
+        ranks = remote_gate.get_rank_remote(rank_name, first_no, last_no)
+        response.all_num = len(ranks.items())
+    response.res.result = True
+
+
+def get_power_rank(first_no, last_no, player, response):
+    if flag_doublu_day():
+        rank_name = 'PowerRank2'
+        last_rank_name = 'PowerRank1'
+    else:
+        rank_name = 'PowerRank1'
+        last_rank_name = 'PowerRank2'
+
+
+def get_star_rank(first_no, last_no, player, response):
+    if flag_doublu_day():
+        rank_name = 'StarRank2'
+        last_rank_name = 'StarRank1'
+    else:
+        rank_name = 'StarRank1'
+        last_rank_name = 'StarRank2'
+
+
+def flag_doublu_day():
+    """
+    return 0 or 1
+    """
+    now = int(time.time())
+    t = time.localtime(now)
+    time1 = time.mktime(time.strptime(time.strftime('%Y-%m-%d 00:00:00', t),
+                        '%Y-%m-%d %H:%M:%S'))
+    return int(time1/(24*60*60)) % 2
--- app/gate/__init__.py
+++ app/gate/__init__.py
@@ -7,6 +7,8 @@ from gtwisted.core import reactor
 from gfirefly.server.globalobject import GlobalObject
 from shared.utils.ranking import Ranking
 from shared.tlog import tlog_action
+from app.gate.redis_mode import tb_character_info
+from shared.utils.const import const
 
 
 front_ip = GlobalObject().json_config['front_ip']
@@ -28,3 +30,30 @@ def tick():
 reactor.callLater(1, tick)
 # 初始化工会排行
 Ranking.init('GuildLevel', 9999)
+Ranking.init('LevelRank1', 99999)
+Ranking.init('LevelRank2', 99999)
+Ranking.init('PowerRank1', 99999)
+Ranking.init('PowerRank2', 99999)
+Ranking.init('StarRank1', 99999)
+Ranking.init('StarRank2', 99999)
+
+
+def add_level_rank_info(instance, users):
+    for uid in users:
+        character_obj = tb_character_info.getObj(uid)
+        character_info = character_obj.hmget(['level', 'attackpoint'])
+        if character_info['attackpoint']:
+            rank_value = character_info['attackpoint']
+        else:
+            rank_value = 0
+        value = character_info['level'] * const.rank_xs + rank_value
+        instance.add(uid, value)  # 添加rank数据
+
+users = tb_character_info.smem('all')
+print users
+
+instance = Ranking.instance('LevelRank1')
+add_level_rank_info(instance, users)
+
+instance = Ranking.instance('LevelRank2')
+add_level_rank_info(instance, users)
