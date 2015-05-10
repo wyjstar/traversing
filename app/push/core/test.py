9ac2e1cb9adb17e21eef2738a30fde93c869e55a
yujinling <yujinling@mobartsgame.com>
2015-05-10 18:29:48 +0800

    1

--- app/push/core/test.py
+++ app/push/core/test.py
@@ -4,20 +4,16 @@ Created on 2015-5-10
 
 @author: hack
 '''
-from app.push.core.apns import APNs
-from app.push.core import APNSWrapper
+from app.push.core.apns import APNs, Frame
 from app.push.core.apns import Payload
-apns = APNs(use_sandbox=True, cert_file='push_dev.pem', enhanced=True)
+apns_handler = APNs(use_sandbox=True, cert_file='push_dev.pem', enhanced=True, ciphers='123456')
 device_token ='8690afe1f1f1067b3f45e0a26a3af4eef5391449e8d07073a83220462bf061be'
 
-wrapper = APNSWrapper.APNSNotificationWrapper('push_dev.pem', True, debug_ssl=True)
-message = APNSWrapper.APNSNotification()
-message.token(device_token)
-message.badge(1)
-message.alert('hello, world')
-wrapper.append(message)
-wrapper.notify()
 
 payload = Payload('hello', badge=1)
 
-apns.gateway_server.send_notification(device_token, payload)
+apns_handler.gateway_server.send_notification(device_token, payload)
+
+frame = Frame()
+frame.add_item(device_token, payload, 1, 1, 1)
+apns_handler.gateway_server.send_notification_multiple(frame)
