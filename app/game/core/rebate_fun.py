ed01ba0007b64c1b22e8881fef0031c462a71802
yujinling <yujinling@mobartsgame.com>
2015-05-30 20:52:10 +0800

    1

--- app/game/core/rebate_fun.py
+++ app/game/core/rebate_fun.py
@@ -74,7 +74,7 @@ def rebate_call(player, recharge_item):
             player.rebate.month_start(recharge_item.get('mailForEver'))
         mail_id, times = player.rebate.month_mails()
         for _ in range(times):
-            send_mail(conf_id=mail_id, receive_id=player._owner.base_info.id)
+            send_mail(conf_id=mail_id, receive_id=player.base_info.id)
         player.rebate.save_data()
 
         notify = rebate_info(player)
