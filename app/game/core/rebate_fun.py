a8fefc28668452708c14616d183a97550cf66f37
yujinling <yujinling@mobartsgame.com>
2015-05-31 19:06:38 +0800

    修改返利卡发送通知邮件

--- app/game/core/rebate_fun.py
+++ app/game/core/rebate_fun.py
@@ -13,47 +13,23 @@ from app.game.action.root import netforwarding
 from app.game.core.mail_helper import send_mail
 remote_gate = GlobalObject().remote.get('gate')
 
-
-def notify_mail(player):
-    """
-    通知可以购买返利卡了
-    """
-    plat = 'ios'
-    if player.base_info.plat_id == 1:
-        plat = 'android'
-    all_rebates = player.rebate.all_rebates()
-    for rid in all_rebates:
-        recharge_item = game_configs.recharge_config[plat].get(rid)
-        if not recharge_item:
-            continue
-        
-        if player.rebate.need_mail(rid, recharge_item.get('giftDays')):
-            if recharge_item.get('giftDays') == 30:
-                mail_id = game_configs.base_config.get('moonCardRemindMail')
-                send_mail(conf_id=mail_id, receive_id=player.base_info.id)
-                player.rebate.send_mail(rid)
-            else:
-                mail_id = game_configs.base_config.get('weekCardRemindMail')
-                send_mail(conf_id=mail_id, receive_id=player.base_info.id)
-            player.rebate.save_data()
-
 def rebate_info(player):
     """
     获取返利卡状态
     """
     
-    notify_mail(player)
     response = rebateInfo()
     plat = 'ios'
     if player.base_info.plat_id == 1:
         plat = 'android'
 
     for rid in game_configs.recharge_config[plat].keys():
-        if game_configs.recharge_config[plat][rid].get('type') == 2:
-            switch, last, can_draw = player.rebate.rebate_status(game_configs.recharge_config[plat][rid].id, game_configs.recharge_config[plat][rid].get('giftDays'))
+        recharge_item = game_configs.recharge_config[plat][rid]
+        if recharge_item.get('type') == 2:
+            switch, last, can_draw = player.rebate.rebate_status(recharge_item.id, recharge_item.get('giftDays'))
             rebate = response.rebates.add()
-            print 'rebate.rid', type(rebate.rid), game_configs.recharge_config[plat][rid].id
-            rebate.rid = game_configs.recharge_config[plat][rid].id
+            print 'rebate.rid', type(rebate.rid), recharge_item.id
+            rebate.rid = recharge_item.id
             rebate.switch = switch
             rebate.last = last
             rebate.draw = can_draw
