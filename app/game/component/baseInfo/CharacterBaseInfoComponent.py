c5888ff7a8779bc5cdd23581c328091b3131a466
wangzhenpu <wangzhenpu@mobartsgame.com>
2015-06-17 17:48:38 +0800

    t

--- app/game/component/baseInfo/CharacterBaseInfoComponent.py
+++ app/game/component/baseInfo/CharacterBaseInfoComponent.py
@@ -168,7 +168,8 @@ class CharacterBaseInfoComponent(Component):
 
         register_time = self._owner.base_info.register_time
         if days_to_current(register_time) > days or \
-            (days_to_current(register_time) == days and string_to_timestamp_hms(ts)) > get_current_timestamp():
+            (days_to_current(register_time) == days and string_to_timestamp_hms(ts)) < get_current_timestamp():
+                logger.error("%s %s" % (string_to_timestamp_hms(ts), get_current_timestamp()))
                 return True
         return False
 
