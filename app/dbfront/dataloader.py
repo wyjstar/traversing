ae4f228df95901c60d3a2e971ed3f91ae9a75cd4
lixin <lixin@mail.com>
2015-07-06 19:26:58 +0800

    fix

--- app/dbfront/dataloader.py
+++ app/dbfront/dataloader.py
@@ -24,8 +24,14 @@ def check_mem_db(delta):
         if char_id >= 10000:
             character_obj = tb_character_info.getObj(char_id)
             character_info = character_obj.hgetall()
+            character_equipments = character_obj.getObj('equipments').hgetall()
+            character_mails = character_obj.getObj('mails').hgetall()
+            character_heroes = character_obj.getObj('heroes').hgetall()
             user_data = dict(id=char_id,
-                             base_info=cPickle.dumps(character_info))
+                             base_info=cPickle.dumps(character_info),
+                             heroes=cPickle.dumps(character_heroes),
+                             mails=cPickle.dumps(character_mails),
+                             equipments=cPickle.dumps(character_equipments))
 
             insert_result = util.InsertIntoDB(CHARACTER_TABLE_NAME, user_data)
             if not insert_result:
@@ -50,7 +56,13 @@ def check_mem_db(delta):
     for char_id in ALL_CHARACTER_IDS:
         character_obj = tb_character_info.getObj(char_id)
         character_info = character_obj.hgetall()
-        user_data = dict(base_info=cPickle.dumps(character_info))
+        character_equipments = character_obj.getObj('equipments').hgetall()
+        character_mails = character_obj.getObj('mails').hgetall()
+        character_heroes = character_obj.getObj('heroes').hgetall()
+        user_data = dict(base_info=cPickle.dumps(character_info),
+                         heroes=cPickle.dumps(character_heroes),
+                         mails=cPickle.dumps(character_mails),
+                         equipments=cPickle.dumps(character_equipments))
         pwere = dict(id=char_id)
         result = util.GetOneRecordInfo(CHARACTER_TABLE_NAME, pwere, ['id'])
         if not result:
