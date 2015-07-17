b1c74bb4951594e62186b0db6e30dccf5712fcf1
lixin <lixin@mail.com>
2015-07-17 15:25:12 +0800

    fix

--- app/dbfront/dataloader.py
+++ app/dbfront/dataloader.py
@@ -20,34 +20,32 @@ def check_mem_db(delta):
     """同步内存数据到数据库
     """
     new_character_ids = tb_character_info.smem('new')
+    new_character_ids = filter(lambda x: x >= 10000, new_character_ids)
     for char_id in new_character_ids:
-        if char_id >= 10000:
-            character_obj = tb_character_info.getObj(char_id)
-            character_info = character_obj.hgetall()
-            character_equipments = character_obj.getObj('equipments').hgetall()
-            character_mails = character_obj.getObj('mails').hgetall()
-            character_heroes = character_obj.getObj('heroes').hgetall()
-            user_data = dict(id=char_id,
-                             base_info=cPickle.dumps(character_info),
-                             heroes=cPickle.dumps(character_heroes),
-                             mails=cPickle.dumps(character_mails),
-                             equipments=cPickle.dumps(character_equipments))
+        character_obj = tb_character_info.getObj(char_id)
+        character_info = character_obj.hgetall()
+        character_equipments = character_obj.getObj('equipments').hgetall()
+        character_mails = character_obj.getObj('mails').hgetall()
+        character_heroes = character_obj.getObj('heroes').hgetall()
+        user_data = dict(id=char_id,
+                         base_info=cPickle.dumps(character_info),
+                         heroes=cPickle.dumps(character_heroes),
+                         mails=cPickle.dumps(character_mails),
+                         equipments=cPickle.dumps(character_equipments))
 
-            insert_result = util.InsertIntoDB(CHARACTER_TABLE_NAME, user_data)
-            if not insert_result:
-                logger.error('insert id:%s error', char_id)
-                result = util.GetOneRecordInfo(CHARACTER_TABLE_NAME,
-                                               dict(id=char_id), ['id'])
-                if result:
-                    tb_character_info.sadd('all', char_id)
-                    tb_character_info.srem('new', char_id)
-            else:
+        insert_result = util.InsertIntoDB(CHARACTER_TABLE_NAME, user_data)
+        if not insert_result:
+            logger.error('insert id:%s error', char_id)
+            result = util.GetOneRecordInfo(CHARACTER_TABLE_NAME,
+                                           dict(id=char_id), ['id'])
+            if result:
                 tb_character_info.sadd('all', char_id)
                 tb_character_info.srem('new', char_id)
-                logger.info('new character:%s', char_id)
-            break
         else:
-            pass
+            tb_character_info.sadd('all', char_id)
+            tb_character_info.srem('new', char_id)
+            logger.info('new character:%s', char_id)
+        break
 
     global ALL_CHARACTER_IDS
     if not ALL_CHARACTER_IDS:
@@ -79,4 +77,7 @@ def check_mem_db(delta):
         ALL_CHARACTER_IDS.remove(char_id)
         break
 
-    reactor.callLater(delta, check_mem_db, delta)
+    if not new_character_ids and not ALL_CHARACTER_IDS:
+        reactor.callLater(delta*10, check_mem_db, delta)
+    else:
+        reactor.callLater(delta, check_mem_db, delta)
