e42934a267fdd47d0387dd9f85d3ec761f644849
wangzhenpu <wangzhenpu@mobartsgame.com>
2015-09-29 11:50:09 +0800

    t

--- app/game/action/node/stage.py
+++ app/game/action/node/stage.py
@@ -291,7 +291,7 @@ def fight_settlement_904(pro_data, player):
     red_units = stage_info.get('red_units')
 
     death_num = len(red_units) - res[2]
-    for i in range(3, 0, -1):
+    for i in range(1, 4):
         star_condition = game_configs.base_config.get('star_condition')
         v = star_condition[i]
         if death_num >= v and res[2] != 0:
