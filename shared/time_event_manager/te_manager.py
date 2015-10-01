f5ab6be81d808dd8c30c5dc35b58e3579ede7f0f
cuichengkai <cuichengkai@mobartsgame.com>
2015-10-02 00:09:29 +0800

    up

--- shared/time_event_manager/te_manager.py
+++ shared/time_event_manager/te_manager.py
@@ -3,6 +3,7 @@
 created by server on 14-7-17下午9:05.
 """
 import time
+from gtwisted.core import reactor
 
 
 class TEManager(object):
@@ -15,7 +16,21 @@ class TEManager(object):
         self._day_events = {}  # {time:[函数]}
 
     def add_event(self, time, type, event):
-        pass
+        """
+        time 当日秒数 时间戳
+        type 1 时间戳 2 当日秒数
+        event 函数
+        """
+        if type == 1:
+            if self._events.get(time):
+                self._events[time].append(event)
+            else:
+                self._events[time] = [event]
+        elif type == 2:
+            if self._day_events.get(time):
+                self._day_events[time].append(event)
+            else:
+                self._day_events[time] = [event]
 
     @property
     def events(self):
@@ -24,3 +39,24 @@ class TEManager(object):
     @events.setter
     def events(self, v):
         self._events = v
+
+    def deal_event(the_time):
+        now = int(time.time())
+        next_time = 0
+        for x, _ in self._events.items():
+            if x <= now or x > now+60:
+                continue
+            if not next_time:
+                next_time = x
+                continue
+            if x < next_time:
+                next_time = x
+        for x, _ in self._day_events.items():
+            if x <= now or x > now+60:
+                continue
+            if not next_time:
+                next_time = x
+                continue
+            if x < next_time:
+                next_time = x
+        reactor.callLater(x, deal_event, time)
