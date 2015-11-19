6208df430d53e8f8d435e0c637dd764be4edc1b1
cuichengkai <cuichengkai@mobartsgame.com>
2015-11-19 21:34:28 +0800

    up

--- app/game/component/character_guild.py
+++ app/game/component/character_guild.py
@@ -135,7 +135,7 @@ class CharacterGuildComponent(Component):
 
     def guild_attr(self):
         guild_level = 1
-        guild_info = game_configs.guild_config.get(guild_level)
+        guild_info = game_configs.guild_config.get(8).get(guild_level)
         if not guild_info:
             return {}
 
