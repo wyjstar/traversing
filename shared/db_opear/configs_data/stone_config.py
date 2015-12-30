bdc675e91f890e5bc4ac83a36b12c91710064936
cuichengkai <cuichengkai@mobartsgame.com>
2015-12-30 15:08:17 +0800

    runt up

--- shared/db_opear/configs_data/stone_config.py
+++ shared/db_opear/configs_data/stone_config.py
@@ -4,6 +4,7 @@ created by server on 14-7-16下午5:48.
 """
 from shared.db_opear.configs_data.common_item import CommonItem
 from shared.db_opear.configs_data import data_helper
+from shared.db_opear.configs_data.data_helper import parse
 
 
 class StoneConfig(object):
