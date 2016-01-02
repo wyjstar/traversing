d7c1d1454dd5578a715a0bc1a34e71c2d41e17ec
wangzhenpu <wangzhenpu@mobartsgame.com>
2016-01-02 17:47:04 +0800

    12

--- app/game/component/character_pay.py
+++ app/game/component/character_pay.py
@@ -141,7 +141,7 @@ class CharacterPay(Component):
             response.res.result = True
             response.gold = self._owner.finance.gold
             response.vip_level = self._owner.base_info.vip_level
-            remote_gate.push_message_remote(2001, self._owner.base_info.id. response.SerializePartialToString())
+            remote_gate.push_message_remote(2001, self._owner.base_info.id, response.SerializePartialToString())
         else:
             self.loop_times += 1
             reactor.callLater(30*self.loop_times, self.recharge)
