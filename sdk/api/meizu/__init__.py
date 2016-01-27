bfb5aaef106095acca0fd0fd447dfb0c61a5868a
lixin <lixin@mail.com>
2016-01-27 19:16:38 +0800

    fix

--- sdk/api/meizu/__init__.py
+++ sdk/api/meizu/__init__.py
@@ -2,14 +2,35 @@
 """
 created by sphinx on
 """
+import time
 import urllib2
+import hashlib
+
 from gfirefly.server.logobj import logger
 
-queryUrl = "https://open-api.flyme.cn/v2/check?"
+queryUrl = "https://api.game.meizu.com/game/security/checksession"
+
+appid = '2912223'
+AppSecret = 'JjGHJ59jhh89OAwX77aUalz3Fqe5Ovcn'
 
 
-def verify_login(token):
-    url = '%saccess_token=%s' % (queryUrl, token)
-    logger.debug('meizu url:%s', url)
-    response = eval(urllib2.urlopen(url).read())
-    return response
+def postUrl(url, data):
+    req = urllib2.Request(url)
+    # data = urllib.urlencode(data)
+    # enable cookie
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    response = opener.open(req, data)
+    return response.read()
+
+
+def verify_login(uid, token):
+    timestamp = int(time.time() * 1000)
+    postdata = 'app_id=%s&session_id=%s&ts=%s&uid=%s' % (appid, token,
+                                                         timestamp, uid)
+    sign = hashlib.md5(postdata + ':' + AppSecret).hexdigest()
+    postdata += '&sign_type=md5&sign=' + sign
+
+    logger.debug('meizu postdata:%s', postdata)
+    result = eval(postUrl(queryUrl, postdata))
+
+    return result['code'] == 200
