1b5bdf595461a77e4ae1208682330d6b4acf4106
cuichengkai <cuichengkai@mobartsgame.com>
2016-02-04 10:09:11 +0800

    fenfu

--- shared/db_opear/configs_data/activity_config.py
+++ shared/db_opear/configs_data/activity_config.py
@@ -8,6 +8,7 @@ from shared.db_opear.configs_data.data_helper import parse
 from shared.db_opear.configs_data.data_helper import convert_keystr2num
 import time
 import re
+from gfirefly.server.globalobject import GlobalObject
 
 
 class ActivityConfig(object):
@@ -16,6 +17,9 @@ class ActivityConfig(object):
 
     def parser(self, config_value):
         for row in config_value:
+            game_server_id = int(GlobalObject().allconfig['server_no'])
+            if row.get('open_zone')[0] != 0 and game_server_id not in row.get('open_zone'):
+                continue
             row["reward"] = parse(row.get("reward"))
             if re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',
                          row["timeStart"]):
