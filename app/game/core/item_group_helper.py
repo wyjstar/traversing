60099ddeae28c63be212220ebab08fd5746ed1be
cuichengkai <cuichengkai@mobartsgame.com>
2016-02-09 18:30:05 +0800

    up

--- app/game/core/item_group_helper.py
+++ app/game/core/item_group_helper.py
@@ -208,14 +208,14 @@ def gain(player, item_group, reason,
         num = int(group_item.num * multiple)
         item_no = group_item.item_no
 
-        multiple2 = 0
+        multiple2 = 1
         for _part_multiple in part_multiple:
             _times = _part_multiple["times"]
             _item_type = _part_multiple["item_type"]
             _item_ids = _part_multiple["item_ids"]
             if type_id == _item_type:
                 if not _item_ids[0] or item_no in _item_ids:
-                    multiple2 += _times
+                    multiple2 = _times
 
         logger.debug("multiple %s multiple2 %s" % (multiple, multiple2))
         num = int(group_item.num * multiple2)
