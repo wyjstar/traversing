aa437791e54d170054b268ec138113ebed7a314a
yinwanqiang <yinwanqiang@mobartsgame.com>
2016-02-18 10:15:50 +0800

    update:add oppo sdk

--- app/game/action/node/sdk_recharge.py
+++ app/game/action/node/sdk_recharge.py
@@ -262,3 +262,28 @@ def vivo_recharge_remote(product_id, fee, order_id, is_online, player):
                                    [player.dynamic_id])
     logger.debug('hauwei response:%s', response)
     return True
+
+@remoteserviceHandle('gate')
+def oppo_recharge_remote(product_id, order_id, is_online, player):
+    logger.debug('oppo_recharge_remote:%s-%s', product_id, order_id)
+    if player.base_info.one_dollar_flowid == order_id:
+        player.base_info.one_dollar_flowid = 'done'
+        logger.debug('one dollar is ok! %s', product_id)
+
+    recharge_item = game_configs.recharge_config.get('android').get(product_id)
+    if recharge_item is None:
+        logger.error('not in rechargeconfig:%s', product_id)
+        return False
+    # if float(fee) != recharge_item.get('currence'):
+    #     logger.error('recharge fee is wrong:%s-%s', fee,
+    #                  recharge_item.get('currence'))
+    #     return False
+
+    response = apple_pb2.AppleConsumeVerifyResponse()
+    response.res.result = True
+    player.recharge.recharge_gain(recharge_item, response, 1)  # 发送奖励邮件
+
+    remote_gate.push_object_remote(12006, response.SerializeToString(),
+                                   [player.dynamic_id])
+    logger.debug('360 response:%s', response)
+    return True
---
+++ app/gate/action/node/oppo.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+"""
+created by sphinx on 11/9/14.
+"""
+import base64
+import json
+from flask import request
+from gfirefly.server.logobj import logger
+from sdk.api.oppo import check_sign
+from gfirefly.server.globalobject import webserviceHandle
+from gfirefly.server.globalobject import GlobalObject
+from app.gate.core.virtual_character_manager import VCharacterManager
+
+
+@webserviceHandle('/oppopay', methods=['post', 'get'])
+def recharge_oppo_response():
+    logger.debug('oppo recharge:%s', request.form)
+    # 验证签名
+    partnerOrder = request.form['partnerOrder']
+    product_id = request.form['partnerOrder']
+    data = {'notifyId':request.form['notifyId'],
+            'partnerOrder':partnerOrder,
+            'productName':request.form['productName'],
+            'productDesc':request.form['productDesc'],
+            'price':request.form['price'],
+            'count':request.form['count'],
+            'attach':request.form['attach'],
+            'sign':request.form['sign']}
+    check_res = check_sign(data)
+    # 验签失败
+    if not check_res:
+        return "result=FAIL&resultMsg=Sign无效"
+    # 发货
+    player_id = int(partnerOrder.split('_')[0])
+    oldvcharacter = VCharacterManager().get_by_id(player_id)
+    if not oldvcharacter:
+        return "result=FAIL&resultMsg=玩家未在线"
+    child_node = GlobalObject().child(oldvcharacter.node)
+    result = child_node.oppo_recharge_remote(
+        oldvcharacter.dynamic_id, product_id, partnerOrder, True)
+    if not result:
+        return "result=FAIL&resultMsg=发货失败"
+
+    return "result=OK"
